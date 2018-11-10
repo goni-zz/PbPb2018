b06e876591adfafab6daf048bd2306db6bfb5f30
goni <geonhee.oh@cern.ch>
2018-11-10 16:58:54 +0100

    update

--- MkBatch/hit0suballruns_Express.sh
+++ MkBatch/hit0suballruns_Express.sh
@@ -15,7 +15,7 @@ done
 
 for i in `cat expressrunstoprocess` 
 do
-	python submitForestExpress.py -q cmscaf1nd -o /eos/cms/store/group/phys_heavyions/dileptons/Data2018/PbPb502TeV/TTrees/Express/v${1}/ -i ExpressPhysics.$i.${1}.list --proxy=proxyforprod
+	python submitForestExpress.py -q 1nd -o /eos/cms/store/group/phys_heavyions/dileptons/Data2018/PbPb502TeV/TTrees/Express/v${1}/ -i ExpressPhysics.$i.${1}.list --proxy=proxyforprod
 done
 
 #rm allruns
